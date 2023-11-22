0e396871e2eb2cf79ff0faa64a1365714b044a1a
feerdd <148528966+feerdd@users.noreply.github.com>
2023-11-22 15:23:21 +0100

    Rain

--- Weather/AllWeatherView.swift
+++ Weather/AllWeatherView.swift
@@ -13,21 +13,7 @@ struct AllWeatherView: View {
             VStack{
                 Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
             }
-            .toolbar{
-                ToolbarItemGroup(placement: .bottomBar){
-                    Button(action: {
-                        
-                    }, label: {
-                        Image(systemName: "map")
-                    })
-                    
-                        Button(action: {
-                            
-                        }, label: {
-                            Image(systemName: "list.bullet")
-                        })
-                }
-            }.background(Color.clear)
+            
             
         }
     }
